dc2dde9fa2834f34bf6ebbdc9d451463d4509bff
Nikhil <nikhilmht33@gmail.com>
2020-09-27 09:06:13 +0530

    Add Kextend library successfully and include an example

--- app/src/main/java/com/nikhil/newsapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/nikhil/newsapp/ui/main/MainActivity.kt
@@ -3,7 +3,7 @@ package com.nikhil.newsapp.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.nikhil.library.LibraryUtil
+import com.nikhil.library.getTimeStamp
 import com.nikhil.newsapp.R
 import com.nikhil.newsapp.ui.TechNewsActivity
 import com.nikhil.newsapp.ui.bitcoin.BitcoinActivity
@@ -14,7 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        LibraryUtil.printStatus()
+        val currentTime = System.currentTimeMillis()
+        println(currentTime.getTimeStamp())
 
         tv_tech_news.setOnClickListener {
             startActivity(Intent(this, TechNewsActivity::class.java))
