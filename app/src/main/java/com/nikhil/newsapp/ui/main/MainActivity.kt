c7ba2bdd6393c87481eb95cb85d440a34f227da3
Nikhil <nikhilmht33@gmail.com>
2020-09-27 08:17:59 +0530

    Add Jitpack Publish example library successfully

--- app/src/main/java/com/nikhil/newsapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/nikhil/newsapp/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nikhil.newsapp.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nikhil.library.LibraryUtil
 import com.nikhil.newsapp.R
 import com.nikhil.newsapp.ui.TechNewsActivity
 import com.nikhil.newsapp.ui.bitcoin.BitcoinActivity
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LibraryUtil.printStatus()
 
         tv_tech_news.setOnClickListener {
             startActivity(Intent(this, TechNewsActivity::class.java))
